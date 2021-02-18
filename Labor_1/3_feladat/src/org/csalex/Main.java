d44d388ca6c151700844d4fdcaa5dbacc280f9b9
Csillag Alex <csillagalex2001@gmail.com>
2021-02-18 09:33:56 +0200

    3. Feladat

--- Labor_1/3_feladat/src/org/csalex/Main.java
+++ Labor_1/3_feladat/src/org/csalex/Main.java
@@ -3,6 +3,13 @@ package org.csalex;
 public class Main {
 
     public static void main(String[] args) {
+        String almafa = "ALMAFA";
 
+        for(int i = 0; i < almafa.length(); i++) {
+            for(int j = 0; j <= i; j++) {
+                System.out.print(almafa.charAt(j));
+            }
+            System.out.println();
+        }
     }
 }
