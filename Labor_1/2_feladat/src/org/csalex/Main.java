92ddc9e66b2a502aa8b55a50e8137c5fc4aade67
Csillag Alex <csillagalex2001@gmail.com>
2021-02-18 09:16:50 +0200

    2. feladat

---
+++ Labor_1/2_feladat/src/org/csalex/Main.java
@@ -0,0 +1,12 @@
+package org.csalex;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    String nev = "Csillag Alex";
+
+	    for(int i = 0; i < nev.length(); i++) {
+            System.out.println(nev.charAt(i));
+        }
+    }
+}
