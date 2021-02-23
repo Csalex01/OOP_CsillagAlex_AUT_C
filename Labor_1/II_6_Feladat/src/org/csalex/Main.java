6ef67a72315621e0554acd43265ab1abff193ed3
Csillag Alex <csillagalex2001@gmail.com>
2021-02-23 16:03:21 +0200

    II_6_Feladat (Nincs befejezve!)

---
+++ Labor_1/II_6_Feladat/src/org/csalex/Main.java
@@ -0,0 +1,30 @@
+package org.csalex;
+
+/*
+
+    EZ A FELADAT NINCS MÉG BEFEJEZVE!
+
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static double[] max2 (double[] array) {
+        double[] max = {Double.NEGATIVE_INFINITY, Double.NEGATIVE_INFINITY};
+
+        if(array.length == 0)
+            return max;
+
+        max[0] = array[0];
+        max[1] = array[1];
+
+        for(int i = 1; i < array.length; i++) {
+
+        }
+
+        return null;
+    }
+}
