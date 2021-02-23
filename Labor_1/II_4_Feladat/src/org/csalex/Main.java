20c3cf894b91d21bbc3bf541c33f4538eff84103
Csillag Alex <csillagalex2001@gmail.com>
2021-02-23 15:02:20 +0200

    II_4_Feladat

---
+++ Labor_1/II_4_Feladat/src/org/csalex/Main.java
@@ -0,0 +1,20 @@
+package org.csalex;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double[] arr = {-4, 3, 2.71, 3.14159, 2.71, 1.6, -1.6};
+
+        System.out.println("Avg: " + mean(arr));
+    }
+
+    private static double mean(double[] array) {
+        double sum = 0;
+
+        for(double el : array) {
+            sum += el;
+        }
+
+        return sum / array.length;
+    }
+}
