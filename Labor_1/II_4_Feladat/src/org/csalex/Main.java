1a513b770f004860acb9f53240795ebe39478c25
Csillag Alex <csillagalex2001@gmail.com>
2021-02-23 15:03:20 +0200

    Corrected II_4_Feladat/.../Main.java

--- Labor_1/II_4_Feladat/src/org/csalex/Main.java
+++ Labor_1/II_4_Feladat/src/org/csalex/Main.java
@@ -4,11 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
         double[] arr = {-4, 3, 2.71, 3.14159, 2.71, 1.6, -1.6};
-
+        
         System.out.println("Avg: " + mean(arr));
     }
 
     private static double mean(double[] array) {
+
+        if(array.length == 0)
+            return Double.NaN;
+
         double sum = 0;
 
         for(double el : array) {
