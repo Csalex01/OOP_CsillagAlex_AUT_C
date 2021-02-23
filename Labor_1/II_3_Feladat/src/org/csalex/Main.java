54083da02792f3fee29e3269c841e06afe8ea682
Csillag Alex <csillagalex2001@gmail.com>
2021-02-23 14:57:01 +0200

    III_3_Feladat

--- Labor_1/II_3_Feladat/src/org/csalex/Main.java
+++ Labor_1/II_3_Feladat/src/org/csalex/Main.java
@@ -3,6 +3,19 @@ package org.csalex;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int number = 127;
+
+        System.out.println(number + ": " + countBits(number));
+    }
+
+    // 13 -> 1101
+    private static int countBits(int number) {
+        int count = 0;
+
+        for(int i = 0; i < 8; i++) {
+            count += (number >> i) & 1;
+        }
+
+        return count;
     }
 }
