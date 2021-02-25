cf7cd0b4482f766052ca57663781367cd43ffc7f
Csillag Alex <csillagalex2001@gmail.com>
2021-02-25 09:40:02 +0200

    Labor 2 - 2. Feladat

---
+++ Labor_2/Feladat/src/rectangle/Main.java
@@ -0,0 +1,8 @@
+package rectangle;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Labor_2/Feladat/src/rectangle/Rectangle.java
@@ -0,0 +1,51 @@
+package rectangle;
+
+public class Rectangle {
+
+    //Attributes, data - tulajdonsagok
+    //egysegbezaras - encapsulation
+    private double width;
+    private double length;
+    //public String rectangleName; //egysegbezaras nincs betartva
+
+    //Methods, behaviour - viselkedes
+
+    //Constructor - inicializalas, lefoglalas
+    //default constructor
+    public Rectangle() {
+
+    }
+
+    public Rectangle(double width, double length) {
+        this.width = width;
+        this.length = length;
+    }
+
+    public Rectangle(double width) {
+        this.width = width;
+        this.length = 0;
+    }
+
+    //Getters
+    public double getWidth(){
+        return width;
+    }
+    public double getLength(){
+        return length;
+    }
+    //Setters
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    @Override
+    public String toString() {
+
+        //felepithetjuk a kimenetet
+        //Rectangle: hossz X szelesseg
+        //Rectangle: 10 X 12
+        return "Rectangle: " + this.width + " X " + this.length;
+    }
+
+
+}
